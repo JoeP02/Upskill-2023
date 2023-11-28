8653409b6706422b887a0bfa0cc385a516433d27
Joe Peirson <b1085143@live.tees.ac.uk>
2023-11-28 12:48:40 +0000

    Changes

--- Source/Upskill_5_1/ShooterCharacter.cpp
+++ Source/Upskill_5_1/ShooterCharacter.cpp
@@ -13,26 +13,6 @@ AShooterCharacter::AShooterCharacter()
 
 }
 
-// Called when the game starts or when spawned
-void AShooterCharacter::BeginPlay()
-{
-	Super::BeginPlay();
-
-	Health = MaxHealth;
-
-	UE_LOG(LogTemp, Warning, TEXT("Health Left Post Setup: %f"), Health);
-}
-
-bool AShooterCharacter::IsDead() const
-{
-	return Health <= 0;
-}
-
-float AShooterCharacter::GetHealthPercent() const
-{
-	return Health / MaxHealth;
-}
-
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -41,91 +21,6 @@ void AShooterCharacter::Tick(float DeltaTime)
 	
 }
 
-// Called to bind functionality to input
-void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AShooterCharacter::MoveForward);
-	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AShooterCharacter::MoveRight);
-	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &APawn::AddControllerPitchInput);
-	PlayerInputComponent->BindAxis(TEXT("LookRight"), this, &APawn::AddControllerYawInput);
-
-	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AShooterCharacter::LookUpRate);
-	PlayerInputComponent->BindAxis(TEXT("LookRightRate"), this, &AShooterCharacter::LookRightRate);
-
-	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &ACharacter::Jump);
-	PlayerInputComponent->BindAction(TEXT("Shoot"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Shoot);
-	// PlayerInputComponent->BindAction(TEXT("Quit"), EInputEvent::IE_Pressed, this, &AShooterCharacter::Quit);
-}
-
-float AShooterCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
-{
-	float DamageToApply = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
-	DamageToApply = FMath::Min(Health, DamageToApply);
-	Health -= DamageToApply;
-	UE_LOG(LogTemp, Warning, TEXT("Health Left: %f"), Health);
-
-	if (IsDead())
-	{
-		DetachFromControllerPendingDestroy();
-		GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	}
-	
-	return DamageToApply;
-}
-
-void AShooterCharacter::SetHealth(float NewHealth)
-{
-	Health = NewHealth;
-}
-
-float AShooterCharacter::GetHealth()
-{
-	return Health;
-}
-
-float AShooterCharacter::GetMaxHealth()
-{
-	return MaxHealth;
-}
-
-
-void AShooterCharacter::MoveForward(float AxisValue)
-{
-	AddMovementInput(GetActorForwardVector() * AxisValue);
-}
-
-void AShooterCharacter::MoveRight(float AxisValue)
-{
-	AddMovementInput(GetActorRightVector() * AxisValue);
-}
-
-void AShooterCharacter::LookUpRate(float AxisValue)
-{
-	AddControllerPitchInput(AxisValue * RotationRate * GetWorld()->DeltaTimeSeconds);
-}
-
-void AShooterCharacter::LookRightRate(float AxisValue)
-{
-	AddControllerYawInput(AxisValue * RotationRate * GetWorld()->DeltaTimeSeconds);
-}
-
-void AShooterCharacter::Quit()
-{
-	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
-
-	if (PlayerController == nullptr)
-	{
-		return;
-	}
-
-	PlayerController->ConsoleCommand(TEXT("quit"));
-}
-
-void AShooterCharacter::Shoot()
-{
-	
-}
 
 
